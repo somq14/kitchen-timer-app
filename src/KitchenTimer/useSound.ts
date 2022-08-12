c25f9fc74699b83e881df996f44f9c3202809c15
somq14 <som.q14@gmail.com>
2022-08-12 12:26:39 +0900

    ⚡️ アラーム音を安定化

--- src/KitchenTimer/useSound.ts
+++ src/KitchenTimer/useSound.ts
@@ -1,7 +1,5 @@
 import { Audio } from "expo-av";
-import { useCallback, useEffect, useMemo, useState } from "react";
-
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+import { useEffect, useMemo, useState } from "react";
 
 export type Sound = {
   pi: () => Promise<void>;
@@ -9,43 +7,54 @@ export type Sound = {
 };
 
 export const useSound = (): Sound | undefined => {
-  const [sound, setSound] = useState<Audio.Sound>();
-
+  const [pi, setPi] = useState<Audio.Sound>();
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-var-requires
     void Audio.Sound.createAsync(require("./pi.mp3")).then((res) =>
-      setSound(res.sound),
+      setPi(res.sound),
+    );
+  }, []);
+
+  const [pipipipi, setPiPiPiPi] = useState<Audio.Sound>();
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-var-requires
+    void Audio.Sound.createAsync(require("./pipipipi.mp3")).then((res) =>
+      setPiPiPiPi(res.sound),
     );
   }, []);
 
   useEffect(() => {
     return () => {
-      if (sound !== undefined) {
-        setSound(undefined);
-        void sound?.unloadAsync();
+      if (pi !== undefined) {
+        setPi(undefined);
+        void pi?.unloadAsync();
       }
     };
-  }, [sound]);
+  }, [pi]);
 
-  const pi = useCallback(async () => {
-    await sound?.replayAsync();
-  }, [sound]);
-
-  const pipipipi = useCallback(() => {
-    const intervalId = setInterval(async () => {
-      await sound?.replayAsync();
-      await sleep(1000);
-      await sound?.replayAsync();
-      await sleep(1000);
-      await sound?.replayAsync();
-      await sleep(1000);
-      await sound?.replayAsync();
-    }, 1000);
-
+  useEffect(() => {
     return () => {
-      clearInterval(intervalId);
+      if (pipipipi !== undefined) {
+        setPi(undefined);
+        void pipipipi?.unloadAsync();
+      }
     };
-  }, [sound]);
+  }, [pipipipi]);
 
-  return useMemo(() => ({ pi, pipipipi }), [pi, pipipipi]);
+  return useMemo(
+    () => ({
+      pi: async () => {
+        await pi?.replayAsync();
+      },
+      pipipipi: () => {
+        const intervalId = setInterval(() => {
+          void pipipipi?.replayAsync();
+        }, 1000);
+        return () => {
+          clearInterval(intervalId);
+        };
+      },
+    }),
+    [pi, pipipipi],
+  );
 };

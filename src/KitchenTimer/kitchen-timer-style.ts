09a04717bc8074d6462e5cc8f62f02bc8aac13f9
somq14 <som.q14@gmail.com>
2022-08-12 12:28:19 +0900

    💄 キッチンタイマーのサイズを調整

--- src/KitchenTimer/kitchen-timer-style.ts
+++ src/KitchenTimer/kitchen-timer-style.ts
@@ -1,6 +1,7 @@
 export const mmToDp = (value: number): number => {
   // Android mdpi は 160 dpi 程度
   // 160 [dpi] = 160 / 25.4 [dot/mm] = 6.29... [dot/mm]
-  const ratio = 160 / 25.4;
+  // 実機で調節し、 140 dpi 想定で表示
+  const ratio = 140 / 25.4;
   return Math.round(ratio * value);
 };
